e253fbc234788fa8e74bef78dad41ad37fba3ccd
Guilherme  Ventura <guilhermeventura@Guilhermes-Mac-Pro.local>
2015-10-29 01:30:40 -0200

    Correção rodapé + inicio pagina sobre

--- sobre.php
+++ sobre.php
@@ -1,2 +1,13 @@
 <?php include("header.html"); ?>
+<section>
+    <img class="banner_home" src="images/banner_sobre.png"/>
+    <article class="a-empresa">
+        <div class="a-empresa-container">
+            <h1>A Vou Cuidar da Minha Marca</h1>
+            <p>
+                É um projeto criado pela agência de design Betha, com o objetivo de ajudar as empresas que<br>estão começando e mesmo as que já estão a algum tempo no mercado, mas querem passar à<br>frente da concorrência                         investindo da melhor forma possível em sua marca.
+            </p>
+        </div>
+    </article>
+</section>
 <?php include("footer.html"); ?>
