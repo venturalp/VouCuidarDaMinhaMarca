32f4b1bcccf243bc3f492a76bb6a4358870a60e4
Guilherme Ventura <venturalp@gmail.com>
2015-11-19 02:36:02 -0200

    Ajuste mobile - logo | Página redes sociais | novos psds

---
+++ redes-sociais.php
@@ -0,0 +1,52 @@
+<?php include("header.html"); ?>
+<img class="banner" src="images/banner_redes.jpg"/>
+<section class="redes1">
+    <h2 class="cinza redes-h2">Toda. Além de ser um meio de comunicação barato, você atinge milhares de pessoas, que podem replicar seu conteúdo para outras milhares de pessoas.</h2>
+    <article class="rede-posicionar">
+            <img src="images/thumbsup.png"/>
+            <img class="rede-quero-posicionar" src="images/quero-posicionar.png"/>
+            <p>
+                - Contratando uma AGÊNCIA ESPECIALIZADA EM MÍDIAS SOCIAIS!
+- Ah! Mas (meu sobrinho, minha esposa, meu funcionário, eu mesmo), que sabe tudo de computador faz isso pra mim de graça!
+- Ok. Mas em uma agência especializada, existe uma equipe que tem grande experiência com este universo, sabe falar com o seu público e ainda, come, dorme, viaja, namora e até vai ao banheiro pensando em como fazer a sua empresa se destacar no meio de tantas empresas que existem por aí.
+            </p>
+    </article>
+    <article class="redes-agencia">
+        <img src="images/mascote.png"/>
+        <p class="redes-agencia-p">O QUE A AGÊNCIA FAZ PELA<br>MINHA EMPRESA?</p>
+
+    </article>
+    <article class="redes-blocos">
+        <div class="redes-bloco bloco-marrom">
+            <div class="redes-header-bloco">
+                <div class="redes-circulo"><p>1</p></div>
+                <h1 class="redes-titulo-bloco">DIAGNOSTICA</h1>
+            </div>
+            <p class="redes-bloco-text">
+                Analisamos os pontos fortes, fracos, ameaças e oportunidades na esfera da sobrevivência, manutenção, crescimento e desenvolvimento
+            </p>
+        </div>
+        <div class="redes-bloco bloco-vermelho-claro">
+            <div class="redes-header-bloco">
+                <div class="redes-circulo"><p>2</p></div>
+                <h1 class="redes-titulo-bloco">PLANEJA</h1>
+            </div>
+            <p class="redes-bloco-text">
+                Baseado nas análises, a agência planeja um cronograma de ações a serem executadas, para que assim, a comunicação seja organizada e direcionada ao público exato, da maneira
+            </p>
+        </div>
+        <div class="redes-bloco bloco-laranja-claro">
+            <div class="redes-header-bloco">
+                <div class="redes-circulo"><p>3</p></div>
+                <h1 class="redes-titulo-bloco">EXECUTA</h1>
+            </div>
+            <p class="redes-bloco-text">
+                Todo o serviço é apresentado a empresa para entendimento e aprovação, após isso a mágica acontece! Sua página é transformada em uma <strong>MÍDIA PODEROSA!</strong>
+            </p>
+        </div>
+    </article>
+
+</section>
+
+<div class="clearfix"></div>
+<?php include("footer.html"); ?>
