7aadfa82d83ffef5ee002f42b28146d303713eb8
Guilherme Ventura <venturalp@gmail.com>
2015-11-24 01:41:52 -0200

    Conteúdo Redes sociais

--- redes-sociais.php
+++ redes-sociais.php
@@ -1,52 +1,92 @@
 <?php include("header.html"); ?>
-<img class="banner" src="images/banner_redes.jpg"/>
-<section class="redes1">
-    <h2 class="cinza redes-h2">Toda. Além de ser um meio de comunicação barato, você atinge milhares de pessoas, que podem replicar seu conteúdo para outras milhares de pessoas.</h2>
-    <article class="rede-posicionar">
-            <img src="images/thumbsup.png"/>
-            <img class="rede-quero-posicionar" src="images/quero-posicionar.png"/>
+    <img class="banner" src="images/banner_redes.jpg" />
+    <section class="redes1">
+        <h2 class="cinza redes-h2">Toda. Além de ser um meio de comunicação barato, você atinge milhares de pessoas, que podem replicar seu conteúdo para outras milhares de pessoas.</h2>
+        <article class="rede-posicionar">
+            <img src="images/thumbsup.png" />
+            <img class="rede-quero-posicionar" src="images/quero-posicionar.png" />
             <p>
-                - Contratando uma AGÊNCIA ESPECIALIZADA EM MÍDIAS SOCIAIS!
-- Ah! Mas (meu sobrinho, minha esposa, meu funcionário, eu mesmo), que sabe tudo de computador faz isso pra mim de graça!
-- Ok. Mas em uma agência especializada, existe uma equipe que tem grande experiência com este universo, sabe falar com o seu público e ainda, come, dorme, viaja, namora e até vai ao banheiro pensando em como fazer a sua empresa se destacar no meio de tantas empresas que existem por aí.
+                - Contratando uma AGÊNCIA ESPECIALIZADA EM MÍDIAS SOCIAIS! - Ah! Mas (meu sobrinho, minha esposa, meu funcionário, eu mesmo), que sabe tudo de computador faz isso pra mim de graça! - Ok. Mas em uma agência especializada, existe uma equipe que tem grande experiência com este universo, sabe falar com o seu público e ainda, come, dorme, viaja, namora e até vai ao banheiro pensando em como fazer a sua empresa se destacar no meio de tantas empresas que existem por aí.
             </p>
-    </article>
-    <article class="redes-agencia">
-        <img src="images/mascote.png"/>
-        <p class="redes-agencia-p">O QUE A AGÊNCIA FAZ PELA<br>MINHA EMPRESA?</p>
+        </article>
+        <article class="redes-agencia">
+            <img src="images/mascote.png" />
+            <p class="redes-agencia-p">O QUE A AGÊNCIA FAZ PELA
+                <br>MINHA EMPRESA?</p>
 
-    </article>
-    <article class="redes-blocos">
-        <div class="redes-bloco bloco-marrom">
-            <div class="redes-header-bloco">
-                <div class="redes-circulo"><p>1</p></div>
-                <h1 class="redes-titulo-bloco">DIAGNOSTICA</h1>
+        </article>
+        <article class="redes-blocos">
+            <div class="redes-bloco bloco-marrom">
+                <div class="redes-header-bloco">
+                    <div class="redes-circulo">
+                        <p>1</p>
+                    </div>
+                    <h1 class="redes-titulo-bloco">DIAGNOSTICA</h1>
+                </div>
+                <p class="redes-bloco-text">
+                    Analisamos os pontos fortes, fracos, ameaças e oportunidades na esfera da sobrevivência, manutenção, crescimento e desenvolvimento
+                </p>
             </div>
-            <p class="redes-bloco-text">
-                Analisamos os pontos fortes, fracos, ameaças e oportunidades na esfera da sobrevivência, manutenção, crescimento e desenvolvimento
-            </p>
-        </div>
-        <div class="redes-bloco bloco-vermelho-claro">
-            <div class="redes-header-bloco">
-                <div class="redes-circulo"><p>2</p></div>
-                <h1 class="redes-titulo-bloco">PLANEJA</h1>
+            <div class="redes-bloco bloco-vermelho-claro">
+                <div class="redes-header-bloco">
+                    <div class="redes-circulo">
+                        <p>2</p>
+                    </div>
+                    <h1 class="redes-titulo-bloco">PLANEJA</h1>
+                </div>
+                <p class="redes-bloco-text">
+                    Baseado nas análises, a agência planeja um cronograma de ações a serem executadas, para que assim, a comunicação seja organizada e direcionada ao público exato, da maneira
+                </p>
             </div>
-            <p class="redes-bloco-text">
-                Baseado nas análises, a agência planeja um cronograma de ações a serem executadas, para que assim, a comunicação seja organizada e direcionada ao público exato, da maneira
-            </p>
-        </div>
-        <div class="redes-bloco bloco-laranja-claro">
-            <div class="redes-header-bloco">
-                <div class="redes-circulo"><p>3</p></div>
-                <h1 class="redes-titulo-bloco">EXECUTA</h1>
+            <div class="redes-bloco bloco-laranja-claro">
+                <div class="redes-header-bloco">
+                    <div class="redes-circulo">
+                        <p>3</p>
+                    </div>
+                    <h1 class="redes-titulo-bloco">EXECUTA</h1>
+                </div>
+                <p class="redes-bloco-text">
+                    Todo o serviço é apresentado a empresa para entendimento e aprovação, após isso a mágica acontece! Sua página é transformada em uma <strong>MÍDIA PODEROSA!</strong>
+                </p>
             </div>
-            <p class="redes-bloco-text">
-                Todo o serviço é apresentado a empresa para entendimento e aprovação, após isso a mágica acontece! Sua página é transformada em uma <strong>MÍDIA PODEROSA!</strong>
-            </p>
-        </div>
-    </article>
+        </article>
+        <h3 class="redes-oque">E O QUE A MINHA EMPRESA GANHA COM ISSO?</h3>
+        <article class="frases">
+            <div class="frases-bloco">
+                <p><b>CREDIBILIDADE</b> – Uma loja de roupas que dá dicas de como lavar corretamente a peça de algodão, ou a loja de material de construção que ensina o tempo de cura do concreto para evitar rachaduras, ganha credibilidade junto aos clientes.</p>
+            </div>
+            <div class="frases-bloco">
+                <p><b>PÚBLICO</b> – Só no Brasil, o Facebook conta com 89 MILHÕES de usuários, o alcance é gigante, desde que seu conteúdo seja relevante.</p>
+            </div>
+            <div class="frases-bloco">
+                <p><b>CONHECIMENTO</b> – seja por feedback de cliente ou dados estatísticos do próprio Facebook, você pode conhecer a fundo seus clientes e até mesmo, possíveis funcionários, através de uma Fan Page (página) bem cuidada, assim, tendo a chance de tomar as melhores decisões para o crescimento da empresa e escolher os rumos a tomar.</p>
+            </div>
+            <div class="frases-bloco">
+                <p><b>INFORMAÇÃO</b> – todos os dados estatísticos de fãs da sua página (sexo, faixa etária, horário que buscam sua página, entre outros dados).</p>
+            </div>
+            <div class="frases-bloco">
+                <p><b>ALCANCE</b> – Você acompanha até onde está chegando o seu conteúdo, se os usuários estão lendo e compartilhando, qual tipo de conteúdo é mais visto e até o dia da semana que sua fan page tem mais acessos ou visualizações.</p>
+            </div>
+            <div class="frases-bloco">
+                <p><b>HISTÓRICO</b> – após a criação da página, você começa uma história e tudo ficará registrado. Você poderá ver os passos que deu, os rumos que tomou, analisar tudo o que passou na sua empresa, pra aprender com isso, e melhorar as decisões futuras.</p>
+            </div>
+            <div class="frases-bloco">
+                <p><b>INTERATIVIDADE</b> – Sua empresa pode conversar com o usuário, descobrir o que estão falando dela, controlar crises e também premiar.</p>
+            </div>
+            <div class="frases-bloco">
+                <p><b>FIDELIDADE</b> – Clientes satisfeitos e que são lembrados, se tornam fiéis e ainda atraem mais clientes para a sua empresa e depois disso, é muito difícil de perdê-los.</p>
+            </div>
+            <div class="frases-bloco">
+                <p><b>AGILIDADE</b> – Com informações exatas em tempo real, você consegue tomar decisões mais rápido e com maior precisão.</p>
+            </div>
+        </article>
+        <article>
+            <h2 class="redes-perfil">Para se manter um perfil em qualquer rede social é preciso de constantes atualizações e capacidade para responder às demandas dos clientes. E a VOU CUIDAR DA MINHA MARCA também                           orienta pequenos empresários, que acreditam na força das redes sociais, mas não sabem como, nem por onde começar.<br><br>
+                    Se você se encaixa nesse perfil, fale com a gente. Estamos aqui para ajudar que o seu negócio ganhe o destaque e o valor que merece, e fique por dentro de tudo o que é dito sobre sua empresa.
+            </h2>
+            <h3 class="cinza txtCenter">ESCOLHA O PLANO QUE MELHOR SE ENCAIXA NO SEU ORÇAMENTO E NECESSIDADE.</h3>
+        </article>
+    </section>
 
-</section>
-
-<div class="clearfix"></div>
-<?php include("footer.html"); ?>
+    <div class="clearfix"></div>
+    <?php include("footer.html"); ?>
