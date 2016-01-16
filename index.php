1cf64d39c40a8f1672647b19dee8fbd66593846c
Guilherme Ventura <venturalp@gmail.com>
2016-01-16 16:46:08 -0200

    btMais home

--- index.php
+++ index.php
@@ -1,82 +1,83 @@
 <?php include("header.html"); ?>
-        <section>
-            <img class="banner" src="images/banner1_home.jpg"/>
-            <ul class="switch">
-                <li class="sw-icon sw-selected"></li>
-                <li class="sw-icon"></li>
-                <li class="sw-icon"></li>
-            </ul>
-        </section>
-        <section class="services">
-            <h1 class="txtCenter cinza">Conheça nossos serviços e faça a diferença.</h1>
-            <div class="services-blocks">
-                <div class="services-block">
-                    <img src="images/icon_registro_red.png"/>
-                    <h1>Registre sua marca</h1>
-                    <p>Sem proteção, sua marca fica vulnerável e pode confundir seu consumidor. Isso não é nada bom para o seu negócio.</p>
-                </div>
-                <div class="services-block">
-                    <img src="images/icon_logo_green.png"/>
-                    <h1>Invista na identidade visual</h1>
-                    <p>Ela é tudo que representa a sua marca ou corporação. Você não vai deixar essa enorme reponsabilidade nas mãos de qualquer um, vai?</p>
-                </div>
-                <div class="services-block">
-                    <img src="images/icon_redes_orange.png"/>
-                    <h1>Apareça nas redes sociais</h1>
-                    <p>Elas são o caminho mais CURTO e mais BARATO para a divulgação de produtos e serviços. O CONTEÚDO gerado por uma empresa, pode ser lido e replicado por milhares de usuários.</p>
-                </div>
-                <div class="services-block">
-                    <img src="images/icon_web_blue.png"/>
-                    <h1>Registre sua marca</h1>
-                    <p>Sem proteção, sua marca fica vulnerável e pode confundir seu consumidor. Isso não é nada bom para o seu negócio.</p>
-                </div>
+    <section>
+        <img class="banner" src="images/banner1_home.jpg" />
+        <ul class="switch">
+            <li class="sw-icon sw-selected"></li>
+            <li class="sw-icon"></li>
+            <li class="sw-icon"></li>
+        </ul>
+    </section>
+    <section class="services">
+        <h1 class="txtCenter cinza">Conheça nossos serviços e faça a diferença.</h1>
+        <div class="services-blocks">
+            <div class="services-block">
+                <img src="images/icon_registro_red.png" />
+                <h1>Registre sua marca</h1>
+                <p>Sem proteção, sua marca fica vulnerável e pode confundir seu consumidor. Isso não é nada bom para o seu negócio.</p>
             </div>
-        </section>
-        <section class="faixa-verde">
-            <p>SUA MARCA TEM QUE MARCAR PRESENÇA E NÃO BOBEIRA.
-        </section>
+            <div class="services-block">
+                <img src="images/icon_logo_green.png" />
+                <h1>Invista na identidade visual</h1>
+                <p>Ela é tudo que representa a sua marca ou corporação. Você não vai deixar essa enorme reponsabilidade nas mãos de qualquer um, vai?</p>
+            </div>
+            <div class="services-block">
+                <img src="images/icon_redes_orange.png" />
+                <h1>Apareça nas redes sociais</h1>
+                <p>Elas são o caminho mais CURTO e mais BARATO para a divulgação de produtos e serviços. O CONTEÚDO gerado por uma empresa, pode ser lido e replicado por milhares de usuários.</p>
+            </div>
+            <div class="services-block">
+                <img src="images/icon_web_blue.png" />
+                <h1>Registre sua marca</h1>
+                <p>Sem proteção, sua marca fica vulnerável e pode confundir seu consumidor. Isso não é nada bom para o seu negócio.</p>
+            </div>
+        </div>
+    </section>
+    <section class="faixa-verde">
+        <p>SUA MARCA TEM QUE MARCAR PRESENÇA E NÃO BOBEIRA.
+    </section>
 
-        <section class="trabalhos">
-            <h1 class="txtCenter cinza">Confira alguns dos trabalhos que já realizamos:</h1>
-            <div class="imgs-trabalhos">
-                <img src="images/icon_cantinho.png"/>
-                <img src="images/icon_jornal.png"/>
-                <img src="images/icon_betha.png"/>
-                <img src="images/icon_aapilpr.png"/>
-                <img src="images/icon_agriar.png"/>
-                <img src="images/icon_chx.png"/>
-                <img src="images/icon_autoestima.png"/>
-                <img src="images/icon_financeira.png"/>
-            </div>
-            <div class="clear"></div>
-        </section>
+    <section class="trabalhos">
+        <h1 class="txtCenter cinza">Confira alguns dos trabalhos que já realizamos:</h1>
+        <div class="imgs-trabalhos">
+            <img src="images/icon_cantinho.png" />
+            <img src="images/icon_jornal.png" />
+            <img src="images/icon_betha.png" />
+            <img src="images/icon_aapilpr.png" />
+            <img src="images/icon_agriar.png" />
+            <img src="images/icon_chx.png" />
+            <img src="images/icon_autoestima.png" />
+            <img src="images/icon_financeira.png" />
+        </div>
+        <div class="btMaisHome"><a  href="clientes.php"><img src="images/boton_home_clientes.fw.png" /></a></div>
+        <div class="clear"></div>
+    </section>
 
-        <section class="clientes">
-            <h1 class="txtCenter cinza">Veja o que alguns de nossos clientes estão dizendo sobre o nosso trabalho:</h1>
-            <div class="cliente-block">
-                <img src="images/icon_cliente1.png"/>
-                <div class="depoimento">
-                    <h3 class="depH1 aspas">Se eu fosse você eu não perderia tempo para contratar.</h3>
-                    <p>"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor."</p>
-                    <h3 class="txtCenter"><span class="depH1">Samuel Pereira,</span> Empresa Tal</h3>
-                </div>
+    <section class="clientes">
+        <h1 class="txtCenter cinza">Veja o que alguns de nossos clientes estão dizendo sobre o nosso trabalho:</h1>
+        <div class="cliente-block">
+            <img src="images/icon_cliente1.png" />
+            <div class="depoimento">
+                <h3 class="depH1 aspas">Se eu fosse você eu não perderia tempo para contratar.</h3>
+                <p>"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor."</p>
+                <h3 class="txtCenter"><span class="depH1">Samuel Pereira,</span> Empresa Tal</h3>
             </div>
-            <div class="cliente-block">
-                <img src="images/icon_cliente1.png"/>
-                <div class="depoimento">
-                    <h3 class="depH1 aspas">Se eu fosse você eu não perderia tempo para contratar.</h3>
-                    <p>"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor."</p>
-                    <h3 class="txtCenter"><span class="depH1">Samuel Pereira,</span> Empresa Tal</h3>
-                </div>
+        </div>
+        <div class="cliente-block">
+            <img src="images/icon_cliente1.png" />
+            <div class="depoimento">
+                <h3 class="depH1 aspas">Se eu fosse você eu não perderia tempo para contratar.</h3>
+                <p>"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor."</p>
+                <h3 class="txtCenter"><span class="depH1">Samuel Pereira,</span> Empresa Tal</h3>
             </div>
-            <div class="cliente-block">
-                <img src="images/icon_cliente1.png"/>
-                <div class="depoimento">
-                    <h3 class="depH1 aspas">Se eu fosse você eu não perderia tempo para contratar.</h3>
-                    <p>"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor."</p>
-                    <h3 class="txtCenter"><span class="depH1">Samuel Pereira,</span> Empresa Tal</h3>
-                </div>
+        </div>
+        <div class="cliente-block">
+            <img src="images/icon_cliente1.png" />
+            <div class="depoimento">
+                <h3 class="depH1 aspas">Se eu fosse você eu não perderia tempo para contratar.</h3>
+                <p>"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor."</p>
+                <h3 class="txtCenter"><span class="depH1">Samuel Pereira,</span> Empresa Tal</h3>
             </div>
-            <div class="clear"></div>
-        </section>
-<?php include("footer.html"); ?>
+        </div>
+        <div class="clear"></div>
+    </section>
+    <?php include("footer.html"); ?>
